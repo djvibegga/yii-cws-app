9289fd9ab3ab47bda2e44d90ed36b6379c66ee2a
Eugeniy Marilev <djvibegga@gmail.com>
2016-02-09 15:13:30 +0200

    + added dump changes

--- db.sql
+++ db.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE IF NOT EXISTS `yii-cws-app` CHARACTER SET UTF8 COLLATE utf8_general_ci;
+USE `yii-cws-app`;
+
 --
 -- Table structure for table `page`
 --
